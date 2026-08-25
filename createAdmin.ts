0c4fff3f3d67c0e208fc9d3d365c20dfca4a0870
Jamal Aldhubhani <greenpro.ksa1@gmail.comVercel@example.com>
2026-08-25 14:40:52 +0300

    Update DB connection and admin user configuration

--- createAdmin.ts
+++ createAdmin.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 async function main() {
   const hashedPassword = await bcrypt.hash('green 2026', 10);
   
-  const user = await prisma.user.upsert({
+  const user1 = await prisma.user.upsert({
     where: { email: 'green' },
     update: {
       password: hashedPassword,
@@ -20,7 +20,24 @@ async function main() {
       role: 'admin',
     },
   });
-  console.log('Admin user created successfully:', user.email);
+
+  const user2 = await prisma.user.upsert({
+    where: { email: 'greenpro.ksa1@gmail.com' },
+    update: {
+      password: hashedPassword,
+      role: 'admin',
+      name: 'Green Projects'
+    },
+    create: {
+      email: 'greenpro.ksa1@gmail.com',
+      name: 'Green Projects',
+      password: hashedPassword,
+      role: 'admin',
+    },
+  });
+
+  console.log('Admin user 1:', user1.email);
+  console.log('Admin user 2:', user2.email);
 }
 
 main()
